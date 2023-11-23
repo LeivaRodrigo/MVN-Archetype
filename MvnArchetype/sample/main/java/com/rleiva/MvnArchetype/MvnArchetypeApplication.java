04332f7549aa42db41a3618cf70d997688a0642e
LeivaRodrigo <leivasebastian98@gmail.com>
2023-11-23 17:04:27 -0300

    Estructura base

---
+++ MvnArchetype/sample/main/java/com/rleiva/MvnArchetype/MvnArchetypeApplication.java
@@ -0,0 +1,13 @@
+package com.rleiva.MvnArchetype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvnArchetypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvnArchetypeApplication.class, args);
+	}
+
+}
---
+++ MvnArchetype/sample/test/java/com/rleiva/MvnArchetype/MvnArchetypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rleiva.MvnArchetype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvnArchetypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
